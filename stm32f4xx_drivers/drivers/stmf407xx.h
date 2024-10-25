59e7c5832abbf5b72188b515e4ba3dfb79f7de66
Athul Muralidhar <athul.m68@gmail.com>
2024-10-25 20:12:42 +0530

    added header files

--- stm32f4xx_drivers/drivers/stmf407xx.h
+++ stm32f4xx_drivers/drivers/stmf407xx.h
@@ -14,7 +14,7 @@
 #define FLASH_BASE_ADDR 							0x08000000U
 #define SRAM1_BASE_ADDR								0x20000000U  						// 112 Kb
 #define SRAM1_SIZE									(uint32_t)((112) * (1024))  		// bytes in hex is 1C000
-#define SRAM1_BASE_ADDR								SRAM1_BASE_ADDR + 1C000  			// 16 Kb
+#define SRAM2_BASE_ADDR								(SRAM1_BASE_ADDR + 1C000)  			// 16 Kb
 #define ROM											0x1FFF0000U
 #define SRAM										SRAM1_BASE_ADDR
 
@@ -36,6 +36,7 @@
 #define GPIOI_BASE_ADDR								(AHB1_PERIPHERAL_BASE_ADDR + 0x2000)
 #define GPIOJ_BASE_ADDR								(AHB1_PERIPHERAL_BASE_ADDR + 0x2400)
 #define GPIOK_BASE_ADDR								(AHB1_PERIPHERAL_BASE_ADDR + 0x2800)
+#define RCC_BASE_ADDR								(AHB1_PERIPHERAL_BASE_ADDR + 0x3800)
 
 // base addresses of peripherals on APB1 bus
 #define SPI2_BASE_ADDR								(APB1_PERIPHERAL_BASE_ADDR + 0x3800)
@@ -61,17 +62,108 @@
 
 // general registers definition for GPIOS
 typedef struct {
-	uint32_t MODER;			// Offset 0x00
-	uint32_t OTYPER;			// Offset 0x04
-	uint32_t OSPEEDR;			// Offset 0x08
-	uint32_t PUPDR;			// Offset 0x0C
-	uint32_t IDR;			// Offset 0x10
-	uint32_t ODR;			// Offset 0x14
-	uint32_t BSRR;			// Offset 0x18
-	uint32_t LCKR;			// Offset 0x1C
-	uint32_t AFRL;			// Offset 0x20
-	uint32_t AFRH;			// Offset 0x24
+	volatile uint32_t MODER;				// Offset 0x00
+	volatile uint32_t OTYPER;				// Offset 0x04
+	volatile uint32_t OSPEEDR;				// Offset 0x08
+	volatile uint32_t PUPDR;				// Offset 0x0C
+	volatile uint32_t IDR;					// Offset 0x10
+	volatile uint32_t ODR;					// Offset 0x14
+	volatile uint32_t BSRR;					// Offset 0x18
+	volatile uint32_t LCKR;					// Offset 0x1C
+	volatile uint32_t AFR[2];				// Offset 0x20-0x24
 } GPIO_RegDef_t;
+
+typedef struct {
+	volatile uint32_t CR;					// Offset 0x00
+	volatile uint32_t PLLCFGR;				// Offset 0x04
+	volatile uint32_t CFGR;					// Offset 0x08
+	volatile uint32_t CIR;					// Offset 0x0C
+	volatile uint32_t AHB1RSTR;				// Offset 0x10
+	volatile uint32_t AHB2RSTR;				// Offset 0x14
+	volatile uint32_t AHB3RSTR;				// Offset 0x18
+	uint32_t RESERVED0;						// Offset 0x1C
+	volatile uint32_t APB1RSTR;				// Offset 0x20
+	volatile uint32_t APB2RSTR;				// Offset 0x24
+	uint32_t RESERVED1[2];					// Offset 0x28-0x2C
+	volatile uint32_t AHB1ENR;				// Offset 0x30
+	volatile uint32_t AHB2ENR;				// Offset 0x34
+	volatile uint32_t AHB3ENR;				// Offset 0x38
+	uint32_t RESERVED2;						// Offset 0x3C
+	volatile uint32_t APB1ENR;				// Offset 0x40
+	volatile uint32_t APB2ENR;				// Offset 0x44
+	uint32_t RESERVED3[2];					// Offset 0x48-0x4C
+	volatile uint32_t AHB1LPENR;			// Offset 0x50
+	volatile uint32_t AHB2LPENR;			// Offset 0x54
+	volatile uint32_t AHB3LPENR;			// Offset 0x58
+	uint32_t RESERVED4;						// Offset 0x5C
+	volatile uint32_t APB1LPENR;			// Offset 0x60
+	volatile uint32_t APB2LPENR;			// Offset 0x64
+	uint32_t RESERVED5[2];					// Offset 0x68-0x6C
+	volatile uint32_t RCC_BDCR;				// Offset 0x70
+	volatile uint32_t RCC_CSR;				// Offset 0x74
+	uint32_t RESERVED6[2];					// Offset 0x78-0x7C
+	volatile uint32_t RCC_SSCGR;			// Offset 0x80
+	volatile uint32_t PLLI2SCFGR;			// Offset 0x84
+} RCC_RegDef_t;
+
+
+// peripheral definitions
+#define GPIOA   	((GPIO_RegDef_t*)GPIOA_BASE_ADDR)
+#define GPIOB   	((GPIO_RegDef_t*)GPIOB_BASE_ADDR)
+#define GPIOC	   	((GPIO_RegDef_t*)GPIOC_BASE_ADDR)
+#define GPIOD   	((GPIO_RegDef_t*)GPIOD_BASE_ADDR)
+#define GPIOE   	((GPIO_RegDef_t*)GPIOE_BASE_ADDR)
+#define GPIOF   	((GPIO_RegDef_t*)GPIOF_BASE_ADDR)
+#define GPIOG   	((GPIO_RegDef_t*)GPIOG_BASE_ADDR)
+#define GPIOH   	((GPIO_RegDef_t*)GPIOH_BASE_ADDR)
+#define GPIOI   	((GPIO_RegDef_t*)GPIOI_BASE_ADDR)
+
+// RCC
+#define RCC			((RCC_RegDef_t*)RCC_BASE_ADDR)
+
+// clock enable macros for GPIOx Peripherals
+#define GPIOA_PCLK_EN()			(RCC->AHB1ENR |=  (1 << 0))
+#define GPIOB_PCLK_EN()			(RCC->AHB1ENR |=  (1 << 1))
+#define GPIOC_PCLK_EN()			(RCC->AHB1ENR |=  (1 << 2))
+#define GPIOD_PCLK_EN()			(RCC->AHB1ENR |=  (1 << 3))
+#define GPIOE_PCLK_EN()			(RCC->AHB1ENR |=  (1 << 4))
+#define GPIOF_PCLK_EN()			(RCC->AHB1ENR |=  (1 << 5))
+#define GPIOG_PCLK_EN()			(RCC->AHB1ENR |=  (1 << 6))
+#define GPIOH_PCLK_EN()			(RCC->AHB1ENR |=  (1 << 7))
+#define GPIOI_PCLK_EN()			(RCC->AHB1ENR |=  (1 << 8))
+
+// clock enable macros for I2Cx Peripherals
+#define I2C1_PCLK_EN()			(RCC->APB1ENR |=  (1 << 21))
+#define I2C2_PCLK_EN()			(RCC->APB1ENR |=  (1 << 22))
+#define I2C3_PCLK_EN()			(RCC->APB1ENR |=  (1 << 23))
+
+
+// clock enable macros for SPIx Peripherals
+#define SPI1_PCLK_EN()			(RCC->APB2ENR |=  (1 << 12))
+#define SPI2_PCLK_EN()			(RCC->APB1ENR |=  (1 << 14))
+#define SPI3_PCLK_EN()			(RCC->APB1ENR |=  (1 << 15))
+#define SPI4_PCLK_EN()			(RCC->APB2ENR |=  (1 << 13))
+
+
+// clock enable macros for USARTx Peripherals
+#define USART1_PCLK_EN()			(RCC->APB2ENR |=  (1 << 4))
+#define USART2_PCLK_EN()			(RCC->APB1ENR |=  (1 << 17))
+#define USART3_PCLK_EN()			(RCC->APB1ENR |=  (1 << 16))
+#define UART4_PCLK_EN()				(RCC->APB1ENR |=  (1 << 19))
+#define UART5_PCLK_EN()				(RCC->APB1ENR |=  (1 << 20))
+#define USART6_PCLK_EN()			(RCC->APB2ENR |=  (1 << 5))
+
+
+// sys cfg clock
+#define SYSCFG_PCLK_EN()			(RCC->APB2ENR |=  (1 << 14))
+
+
+
+
+
+
+
+
 
 
 
